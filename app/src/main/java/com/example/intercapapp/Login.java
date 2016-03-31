988f81240983c8037504b68b7150b89ea94675c9
Matias <mbascolo@gmail.com>
2016-03-31 11:27:29 -0300

    Login con BBDD Intercap

--- app/src/main/java/com/example/intercapapp/Login.java
+++ app/src/main/java/com/example/intercapapp/Login.java
@@ -1,171 +1,200 @@
 package com.example.intercapapp;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mysqltest.R;
 
-public class Login extends Activity implements OnClickListener {
+public class Login extends AppCompatActivity {
 
-	private EditText user, pass;
-	private Button mSubmit, mRegister;
+	Activity context;
+	ProgressDialog pd;
 
-	// Progress Dialog
-	private ProgressDialog pDialog;
-
-	// JSON parser class
-	JSONParser jsonParser = new JSONParser();
-
-	// php login script location:
-
-	// localhost :
-	// testing on your device
-	// put your local ip instead, on windows, run CMD > ipconfig
-	// or in mac's terminal type ifconfig and look for the ip under en0 or en1
-	// private static final String LOGIN_URL =
-	// "http://xxx.xxx.x.x:1234/webservice/login.php";
-
-	// testing on Emulator:
-	private static final String LOGIN_URL = "http://www.beansoft.com.ar/webservusers/login.php";
-
-	// testing from a real server:
-	// private static final String LOGIN_URL =
-	// "http://www.mybringback.com/webservice/login.php";
-
-	// JSON element ids from repsonse of php script:
-	private static final String TAG_SUCCESS = "success";
-	private static final String TAG_MESSAGE = "message";
+	TextView nombre_usuario,pass_usuario;
+	private CheckBox check_recordar;
+	//String ESTADO_LOGIN = "A" ;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		/*if (ESTADO_LOGIN == "A"){
+			check_recordar = (CheckBox)findViewById(R.id.checkBoxRecordar);*/
+			nombre_usuario = (TextView)findViewById(R.id.username);
+			pass_usuario = (TextView)findViewById(R.id.password);
+			context=this;
+			//CargarPreferencias();
 
-		// setup input fields
-		user = (EditText) findViewById(R.id.username);
-		pass = (EditText) findViewById(R.id.password);
+		//} else if (ESTADO_LOGIN == "B"){
 
-		// setup buttons
-		mSubmit = (Button) findViewById(R.id.login);
-		mRegister = (Button) findViewById(R.id.register);
+			//Intent principal = new Intent(Login.this,HomeActivity.class);
+			//finish();
+			//startActivity(principal);
+		//}
 
-		// registrarse listeners
-		mSubmit.setOnClickListener(this);
-		mRegister.setOnClickListener(this);
 
 	}
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		switch (v.getId()) {
-		case R.id.login:
-			new AttemptLogin().execute();
-			break;
-		case R.id.register:
-			Intent i = new Intent(this, Registrarse.class);
-			startActivity(i);
-			break;
 
-		default:
-			break;
-		}
+	public void CargarPreferencias(){
+		//Guarda las preferencias en un xml llamado PreferenciasUsuario
+		SharedPreferences misPreferencias = getSharedPreferences("PreferenciasUsuario", Context.MODE_PRIVATE);
+		check_recordar.setChecked(misPreferencias.getBoolean("checked", false));
+		nombre_usuario.setText(misPreferencias.getString("nombre",""));
+		pass_usuario.setText(misPreferencias.getString("pass", ""));
+
+
 	}
 
-	class AttemptLogin extends AsyncTask<String, String, String> {
+	public void GuardarPreferencias(){
 
-		@Override
-		protected void onPreExecute() {
+		SharedPreferences misPreferencias = getSharedPreferences("PreferenciasUsuario", Context.MODE_PRIVATE);
+		SharedPreferences.Editor editor = misPreferencias.edit();
+		boolean valor = check_recordar.isChecked();
+		String nombre = nombre_usuario.getText().toString();
+		String pass = pass_usuario.getText().toString();
+		editor.putBoolean("checked", valor);
+		editor.putString("nombre", nombre);
+		editor.putString("pass", pass);
+		editor.commit();
+	}
+
+
+	//Botón Ingresar
+	public void OnClick(View v){
+		BackTask bt=new BackTask();
+		bt.execute("http://www.intercapweb.com.ar/TiendaVirtualv3/rs/usuario/login/" + nombre_usuario.getText() + "/" + pass_usuario.getText());
+	}
+
+	public void OnCheck (View v){
+		GuardarPreferencias();
+	}
+
+
+	//background process to download the file from internet
+	private class BackTask extends AsyncTask<String,Integer,Void>{
+		String text="";
+		protected void onPreExecute(){
 			super.onPreExecute();
-			pDialog = new ProgressDialog(Login.this);
-			pDialog.setMessage("Intentando inicio de sesión...");
-			pDialog.setIndeterminate(false);
-			pDialog.setCancelable(true);
-			pDialog.show();
+			//display progress dialog
+			pd = new ProgressDialog(context);
+			pd.setTitle("Comprobando datos");
+			pd.setMessage("Espere por favor.");
+			pd.setCancelable(true);
+			pd.setIndeterminate(false);
+			pd.show();
+
 		}
 
-		@Override
-		protected String doInBackground(String... args) {
-			// TODO Auto-generated method stub
-			// Check for success tag
-			int success;
-			String username = user.getText().toString();
-			String password = pass.getText().toString();
+
+
+		protected Void doInBackground(String...params){
+			URL url;
 			try {
-				// Construyendo parámetros
-				List<NameValuePair> params = new ArrayList<NameValuePair>();
-				params.add(new BasicNameValuePair("username", username));
-				params.add(new BasicNameValuePair("password", password));
+				//create url object to point to the file location on internet
+				url = new URL(params[0]);
+				//make a request to server
+				HttpURLConnection con=(HttpURLConnection)url.openConnection();
+				//get InputStream instance
+				InputStream is=con.getInputStream();
+				//create BufferedReader object
+				BufferedReader br=new BufferedReader(new InputStreamReader(is));
+				String line;
+				//read content of the file line by line
+				while((line=br.readLine())!=null){
+					text+=line;
 
-				Log.d("request!", "starting");
-				// getting product details by making HTTP request
-				JSONObject json = jsonParser.makeHttpRequest(LOGIN_URL, "POST",
-						params);
-
-				// check your log for json response
-				Log.d("Intentando ingresar", json.toString());
-
-				// json success tag
-				success = json.getInt(TAG_SUCCESS);
-				if (success == 1) {
-					Log.d("Login correcto!", json.toString());
-					// save user data
-					SharedPreferences sp = PreferenceManager
-							.getDefaultSharedPreferences(Login.this);
-					Editor edit = sp.edit();
-					edit.putString("username", username);
-					edit.commit();
-					
-					Intent i = new Intent(Login.this, HomeActivity.class);
-					finish();
-					startActivity(i);
-					return json.getString(TAG_MESSAGE);
-				} else {
-					Log.d("Fallo en login!", json.getString(TAG_MESSAGE));
-					return json.getString(TAG_MESSAGE);
 				}
-			} catch (JSONException e) {
+
+				br.close();
+
+			}catch (Exception e) {
 				e.printStackTrace();
+				//close dialog if error occurs
+				if(pd!=null) pd.dismiss();
 			}
 
 			return null;
 
 		}
 
-		protected void onPostExecute(String file_url) {
-			// dismiss the dialog once product deleted
-			pDialog.dismiss();
-			if (file_url != null) {
+
+		protected void onPostExecute(Void result){
+			//close dialog
+			if(pd!=null)
+				pd.dismiss();
 
 
-				Toast.makeText(Login.this, file_url, Toast.LENGTH_LONG).show();
+			//display read text in TextVeiw
+			//resultado.setText(text);
+
+			if(text.equals("Autorizado")){
+
+				//GuardarPreferencias();
+				Intent principal = new Intent(Login.this,HomeActivity.class);
+				finish();
+				startActivity(principal);
+
+
+			} else if (text.equals("Password incorrecto")) {
+
+				Toast.makeText(Login.this,"Datos incorrectos",Toast.LENGTH_SHORT).show();
+
+			} else if (text.equals("")) {
+
+				Toast.makeText(Login.this,"Ingrese su usuario y contraseNa por favor.",Toast.LENGTH_SHORT).show();
 			}
 
 		}
 
+
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_form_reserva, menu);
+		return true;
+	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 }
